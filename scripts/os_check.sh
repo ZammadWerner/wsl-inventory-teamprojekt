f5ca97331262514ba6298f28ee62a2dc69a6884f
Werner <werner.hause@vodafonemail.de>
2026-08-26 10:18:19 +0200

    Ticket vollstaendig umgesetzt

--- scripts/os_check.sh
+++ scripts/os_check.sh
@@ -39,3 +39,30 @@ uname -r
 echo ""
 echo "System-Architektur:"
 uname -m
+#!/bin/bash
+echo "Betriebssystem"
+echo "=============="
+echo "Hostname:"
+hostname
+echo ""
+echo "Aktuelles Datum & Uhrzeit:"
+date
+echo ""
+echo "Betriebssystem-Version:"
+grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2- | tr -d '"'
+echo ""
+echo "WSL-Erkennung:"
+if grep -qi microsoft /proc/version; then
+    echo "System laeuft unter WSL"
+else
+    echo "System laeuft unter nativem Linux"
+fi
+echo ""
+echo "CPU-Modell:"
+grep -m1 "model name" /proc/cpuinfo
+echo ""
+echo "Kernel-Version:"
+uname -r
+echo ""
+echo "System-Architektur:"
+uname -m
