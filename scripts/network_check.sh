1194bcdf9193f60b2f3595b0ee7b5eaa0cabe134
Werner <werner.hause@vodafonemail.de>
2026-08-26 10:31:22 +0200

    Erste Erweiterung fuer Ticket C umgesetzt

--- scripts/network_check.sh
+++ scripts/network_check.sh
@@ -9,3 +9,14 @@ hostname -I
 echo ""
 echo "Standardroute:"
 ip route | grep default
+#!/bin/bash
+echo "Netzwerk"
+echo "========"
+echo "Hostname:"
+hostname
+echo ""
+echo "Netzwerkschnittstellen (kompakt):"
+ip -brief addr
+echo ""
+echo "Standardroute:"
+ip route | grep default
