d63aaec019ae4b62996a14a486d0548e08b67d4c
Werner <werner.hause@vodafonemail.de>
2026-08-26 10:36:23 +0200

    Erste Erweiterung fuer Ticket D umgesetzt

--- scripts/process_check.sh
+++ scripts/process_check.sh
@@ -9,3 +9,20 @@ id
 echo ""
 echo "Erste laufende Prozesse:"
 ps -e | head
+#!/bin/bash
+echo "Benutzer und Prozesse"
+echo "======================"
+echo "Aktueller Benutzer:"
+whoami
+echo ""
+echo "Benutzer-ID:"
+id
+echo ""
+echo "Home-Verzeichnis des Benutzers:"
+echo "$HOME"
+echo ""
+echo "Verwendete Shell:"
+echo "$SHELL"
+echo ""
+echo "Gesamtzahl der laufenden Prozesse:"
+ps -e --no-headers | wc -l
