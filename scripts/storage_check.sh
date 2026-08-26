bab9ce594ae8269933baecd79972e1d207a02e1f
Werner <werner.hause@vodafonemail.de>
2026-08-26 10:24:25 +0200

    Erste Erweiterung fuer Ticket B umgesetzt

--- scripts/storage_check.sh
+++ scripts/storage_check.sh
@@ -10,3 +10,17 @@ free -h
 echo ""
 echo "Home-Verzeichnis:"
 echo "$HOME"
+#!/bin/bash
+echo "Speicher und Dateisystem"
+echo "========================"
+echo "Festplattenbelegung und Dateisystemtyp:"
+df -hT /
+echo ""
+echo "Inode-Belegung des Wurzelverzeichnisses:"
+df -i /
+echo ""
+echo "Arbeitsspeicher:"
+free -h
+echo ""
+echo "Home-Verzeichnis:"
+echo "$HOME"
